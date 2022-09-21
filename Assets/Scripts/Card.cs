2e575bbb2e2edf6e0a2c6085ec517247f1a7ffd7
gerald <gombirog@gmail.com>
2022-09-21 12:48:40 +0200

    Added holding card hand and hovering upon on mouse over

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -13,11 +14,23 @@ public class Card : MonoBehaviour
     public TMP_Text healthText, attackText, costText, nameText, actionText, loreText;
 
     public Image characterImage, backgroundImage = null;
+
+    public float moveSpeed = 5f, rotateSpeed = 540f;
+
+    public bool inHand = false;
+    
+    public int handPosition;
+
+    private Vector3 _targetPosition;
+    private Quaternion _targetRotation;
+    private HandController _handController = null;
     
     // Start is called before the first frame update
     void Start()
     {
         SetupCard();
+
+        _handController = FindObjectOfType<HandController>();
     }
 
     public void SetupCard()
@@ -41,6 +54,30 @@ public class Card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, moveSpeed * Time.deltaTime);
+        transform.rotation =
+            Quaternion.RotateTowards(transform.rotation, _targetRotation, rotateSpeed * Time.deltaTime); 
+    }
+
+    public void MoveToPosition(Vector3 positionToMoveTo, Quaternion targetRotation)
+    {
+        _targetPosition = positionToMoveTo;
+        _targetRotation = targetRotation;
+    }
+
+    private void OnMouseOver()
+    {
+        if (inHand)
+        {
+            MoveToPosition(_handController.cardPositions[handPosition] + new Vector3(0f,0.5f,0.7f), _targetRotation);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (inHand)
+        {
+            MoveToPosition(_handController.cardPositions[handPosition], _targetRotation);
+        }
     }
 }
